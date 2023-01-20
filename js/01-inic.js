f9c13a778ede425c72821d75c111a255922ba7a5
IrinaVaronka <varonkairina@gmail.com>
2023-01-20 10:52:34 +0200

    Kintamųjų inicijavimas

---
+++ js/01-inic.js
@@ -0,0 +1,61 @@
+console.clear()
+
+/*
+1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+    a. Po kiekvieno jų inicijavimo, išvesti į console
+2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+    a. Po kiekvieno jų inicijavimo, išvesti į console
+3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+    a. Po kiekvieno jų inicijavimo, išvesti į console
+4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+    a. Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+console.log('--1---');
+
+const number = 679;
+console.log(number);
+
+const metai = 25;
+console.log(metai);
+
+const savaite = 6;
+console.log(savaite);
+
+console.log('--2---');
+
+const firstName = 'Iryna';
+console.log(firstName);
+
+const secondName = 'Varonka';
+console.log(secondName);
+
+const miestas = 'Druskininkai';
+console.log(miestas);
+
+console.log('--3---');
+
+const years = [2019, 2020, 2021, 2022, 2023];
+console.log(years);
+
+const price = [13, 18, 9, 4, 8];
+console.log(price);
+
+const flatsNumbers = [45, 46, 47, 48, 49]
+console.log(flatsNumbers);
+
+
+console.log('--4---');
+
+const days = ['Sunday', 'Monday', 'Friday', 'Tuesday', 'Saturday'];
+console.log(days);
+
+const animals = ['dog','cat', 'rat', 'bear', 'horse']
+console.log(animals);
+
+const cities = ['London', 'Berlin', 'Vilnius', 'Tallin', 'Viena'];
+console.log(cities);
+
+
+
+
