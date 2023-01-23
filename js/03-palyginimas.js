0781149540881fbb0747d6263fc7c79e650baf47
IrinaVaronka <varonkairina@gmail.com>
2023-01-23 14:00:23 +0200

    palyginimas_2

--- js/03-palyginimas.js
+++ js/03-palyginimas.js
@@ -15,7 +15,6 @@ f. kuris mažesnis arba lygus
 */
 
 console.log('---1---');
-console.log();
 
 if (36 > 34) {
     console.log('Pomidoras');
@@ -50,3 +49,96 @@ if (5 <= 11) {
 } else {
     console.log('Bandykite kitą kartą.');
 }
+
+
+// 2. Išvesti teksto tipo kintamųjų ilgius
+console.log('---2---');
+
+
+let tekstas = 'road';
+console.log(tekstas.length);
+
+/* Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+a. kuris didesnis
+b. kuris mažesnis
+c. ar jie lygūs
+d. ar jie nelygūs
+e. kuris didesnis arba lygus
+f. kuris mažesnis arba lygus
+*/
+console.log('---3---');
+
+const a = 'vasara';
+const b = 'pavasaris';
+
+if (a.length > b.length) {
+    console.log('Pomidoras');
+} else if (a.length < b.length) {
+    console.log('Bandykite kitą kartą.');
+}
+
+if (a.length === b.length) {
+    console.log('Pomidoras');
+} 
+if (a.length !== b.length) {
+    console.log('Bandykite kitą kartą.');
+}
+
+if (a.length >= b.length) {
+    console.log('Pomidoras');
+} 
+if (a.length <= b.length) {
+    console.log('Bandykite kitą kartą.');
+}
+
+
+// 4. Išvesti sąrašo tipo kintamųjų ilgius.
+console.log('---4---');
+
+let list = ['a', 'b', 'c'];
+
+console.log(list.length);
+
+/* 5. Tarpusavyje palyginti sąrašo tipo kintamųjų ilgius:
+a. kuris didesnis
+b. kuris mažesnis
+c. ar jie lygūs
+d. ar jie nelygūs
+e. kuris didesnis arba lygus
+f. kuris mažesnis arba lygus
+*/
+console.log('---5---');
+
+const list1 = [7, 8, 9, 7];
+const list2 = ['hello', 'hi', 'labas', 'hola'];
+
+if (list1.length > list2.length) {
+    console.log('true didesnis','-->', 'Pomidoras');
+} else {
+    console.log('false didesnis','-->', 'Bandykite kitą kartą.');
+}
+if (list1.length < list2.length) {
+    console.log('true mažesnis','-->', 'Pomidoras');
+} else {
+    console.log('false mažesnis','-->', 'Bandykite kitą kartą.');
+}
+if (list1.length === list2.length) {
+    console.log('true lygūs','-->','Pomidoras');
+} else {
+    console.log('false lygūs','-->', 'Bandykite kitą kartą.');
+}
+
+if (list1.length >= list2.length) {
+    console.log('true didesnis arba lygus','-->', 'Pomidoras');
+} else {
+    console.log('false didesnis arba lygus','-->','Bandykite kitą kartą.');
+}
+if (list1.length <= list2.length) {
+    console.log('true mažesnis arba lygus','-->','Pomidoras');
+} else {
+    console.log('false mažesnis arba lygus','-->', 'Bandykite kitą kartą.');
+}
+
+
+
+
