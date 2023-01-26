2e185756404be6dbf295acc02a7ff8e72417b0d6
IrinaVaronka <varonkairina@gmail.com>
2023-01-26 19:44:10 +0200

    codewars-oppositeNumbers

---
+++ js/108-oppositeNumber.js
@@ -0,0 +1,18 @@
+console.clear()
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+    let negative = number;
+    let neg2 = -negative
+   return neg2;
+  }
+  console.log(opposite(1), '-->', -1);
