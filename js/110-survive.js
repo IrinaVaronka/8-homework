bc3cf9ff30830391d17e655b6c802d0d58855d5c
IrinaVaronka <varonkairina@gmail.com>
2023-01-27 14:23:42 +0200

    codewars-survive

---
+++ js/110-survive.js
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+function hero(bullets, dragons){
+
+    if (bullets >= dragons *2) {
+    return true;
+    } else {
+        if (bullets < dragons * 2) {
+          return false;  
+        }
+    }
+}
+   console.log(hero(10, 5), '-->', true);
+   console.log(hero(7, 4), '-->', false);
+    console.log(hero(4, 5), '-->', false);
+
+
+    //else from codewars:
+
+    function hero(bullets, dragons){
+        return (bullets / 2 >= dragons) ? true : false;
+        }
